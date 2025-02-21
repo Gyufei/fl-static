286113cd4774e083cfb0a9a9a80a05d026687ff4
fei guo <812773673@qq.com>
2025-02-21 19:21:04 +0800

    feat:new-footer

--- app/[locale]/_home/contact-us/index.tsx
+++ app/[locale]/_home/contact-us/index.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { useTranslations } from "next-intl";
 import SubscribeInput from "./subscribe-input";
 import SocialIcon from "./social-icon";
+import { Link } from "@/app/navigation";
+import { DocLink } from "@/lib/utils/jump-url";
 
 export default function ContactUs() {
   const t = useTranslations("Home");
 
   return (
-    <div className="bg-primaryColor px-4 pt-10 sm:px-[120px] sm:pt-[60px]">
+    <div className="bg-primaryColor bg-none bg-no-repeat px-4 pt-10 sm:bg-[url('/img/home/footer-bg.svg')] sm:bg-[bottom_right_170px] sm:px-[120px] sm:pt-[60px]">
       <div className="flex flex-col items-end justify-between sm:flex-row">
         <div className="flex w-full flex-col items-center sm:w-fit sm:items-start">
           <div className="text-center text-2xl font-medium leading-9 text-[#fff] sm:text-4xl sm:text-[40px] sm:leading-[54px]">
@@ -54,11 +56,42 @@ export default function ContactUs() {
         </div>
       </div>
 
-      <div className="flex items-center justify-center px-0 pb-5 pt-8 sm:flex-row sm:items-center sm:justify-start sm:pb-[60px] sm:pt-[60px]">
-        <Image src="/icons/logo-white.svg" width={75} height={20} alt="logo" />
+      <div className="my-[30px] flex flex-col items-center justify-center gap-y-[30px] sm:mb-[20px] sm:mt-[60px] sm:flex-row sm:items-center sm:justify-between sm:gap-y-0">
+        <div className="flex items-center justify-center">
+          <Image
+            src="/icons/logo-white.svg"
+            width={75}
+            height={20}
+            alt="logo"
+          />
+          <div className="mx-5 h-4 w-[1px] bg-white"></div>
+          <div className="flex items-center justify-center gap-x-[10px]">
+            <span className="text-base leading-6 text-white">
+              {t("cap-BuiltOn")}
+            </span>
+            <Image src="/icons/monad.svg" width={108} height={20} alt="monad" />
+          </div>
+        </div>
+        <div className="flex items-center justify-center gap-x-[30px] sm:gap-x-14">
+          <Link href="/" className="text-sm leading-5 text-white">
+            {t("btn-Contact")}
+          </Link>
+          <Link href={DocLink} className="text-sm leading-5 text-white">
+            {t("btn-Learn")}
+          </Link>
+          <Link href={DocLink} className="text-sm leading-5 text-white">
+            {t("btn-Developers")}
+          </Link>
+        </div>
       </div>
 
-      <div className="flex h-[60px] items-center justify-center border-t border-[#ffffff30] sm:justify-start">
+      <div className="flex flex-col-reverse items-center justify-center gap-y-[10px] border-t border-[#ffffff30] py-5 sm:flex-row sm:justify-between sm:gap-y-0 sm:pb-[26px] sm:pt-6">
+        <div className="flex items-center gap-x-[10px]">
+          <div className="h-3 w-3 rounded-full bg-[#06D98B]"></div>
+          <div className="text-sm leading-5 text-[#ffffff60]">
+            {t("p-AllSystemsOperational")}
+          </div>
+        </div>
         <div className="text-sm leading-5 text-[#ffffff60]">
           <span className="inline-block">
             Copyright @ Whity Ltd 2024. All Rights Reserved.
